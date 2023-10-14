37d28c597d5a16da1092bf14f606c7a0fccbe389
vishwas1307 <147929456+vishwas1307@users.noreply.github.com>
2023-10-14 19:52:38 +0530

    Assignment2

---
+++ Assignment2.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Counts (org TEXT, count INTEGER);
+
+INSERT INTO counts (org , count ) VALUES ('iupui.edu', 536);
+INSERT INTO counts (org , count ) VALUES ('umich.edu', 491);
+INSERT INTO counts (org , count ) VALUES ('indiana.edu', 178);
+INSERT INTO counts (org , count ) VALUES ('caret.cam.ac.uk', 157);
+INSERT INTO counts (org , count ) VALUES ('vt.edu' ,110);
+INSERT INTO counts (org , count ) VALUES ('uct.ac.za', 96);
+INSERT INTO counts (org , count ) VALUES ('media.berkeley.edu', 56);
+INSERT INTO counts (org , count ) VALUES ('ufp.pt',28);
+INSERT INTO counts (org , count ) VALUES ('gmail.com', 25);
+INSERT INTO counts (org , count ) VALUES ('et.gatech.edu', 17);
+
+select * from counts
+
+'iupui.edu', '536'
+'umich.edu', '491'
+'indiana.edu', '178'
+'caret.cam.ac.uk', '157'
+'vt.edu', '110'
+'uct.ac.za', '96'
+'media.berkeley.edu', '56'
+'ufp.pt', '28'
+'gmail.com', '25'
+'et.gatech.edu', '17'
